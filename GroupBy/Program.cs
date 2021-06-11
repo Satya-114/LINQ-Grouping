517d4a63159c45255761adff9c263cf6454c7a09
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 09:13:23 +0530

    Add files via upload

---
+++ GroupBy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_GroupBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var GroupByMS = Student.GetStudents().GroupBy(s => s.Branch);
+            //Using Query Syntax
+            IEnumerable<IGrouping<string, Student>> GroupByQS = (from std in Student.GetStudents()
+                                                                 group std by std.Branch);
+            //It will iterate through each groups
+            foreach (var group in GroupByMS)
+            {
+                Console.WriteLine(group.Key + " : " + group.Count());
+                //Iterate through each student of a group
+                foreach (var student in group)
+                {
+                    Console.WriteLine("  Name :" + student.Name + ", Age: " + student.Age + ", Gender :" + student.Gender);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
