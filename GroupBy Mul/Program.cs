8b31428d440b1e86137eca53dcdd1e675b673c24
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 09:15:40 +0530

    Add files via upload

---
+++ GroupBy Mul/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace GroupBy_MUL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var GroupByMultipleKeysMS = Student.GetStudents()
+                                         .GroupBy(x => new { x.Branch, x.Gender })
+                                         .OrderByDescending(g => g.Key.Branch).ThenBy(g => g.Key.Gender)
+                                         .Select(g => new
+                                         {
+                                             Branch = g.Key.Branch,
+                                             Gender = g.Key.Gender,
+                                             Students = g.OrderBy(x => x.Name)
+                                         });
+            //Using Query Syntax
+            var GroupByMultipleKeysQS = from student in Student.GetStudents()
+                                        group student by new
+                                        {
+                                            student.Branch,
+                                            student.Gender
+                                        } into stdGroup
+                                        orderby stdGroup.Key.Branch descending,
+                                                stdGroup.Key.Gender ascending
+                                        select new
+                                        {
+                                            Branch = stdGroup.Key.Branch,
+                                            Gender = stdGroup.Key.Gender,
+                                            Students = stdGroup.OrderBy(x => x.Name)
+                                        };
+            //It will iterate through each group
+            foreach (var group in GroupByMultipleKeysQS)
+            {
+                Console.WriteLine($"Barnch : {group.Branch} Gender: {group.Gender} No of Students = {group.Students.Count()}");
+                //It will iterate through each item of a group
+                foreach (var student in group.Students)
+                {
+                    Console.WriteLine($"  ID: {student.ID}, Name: {student.Name}, Age: {student.Age} ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ GroupBy Mul/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupBy_MUL
+{
+    class Student
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Branch { get; set; }
+        public int Age { get; set; }
+        public static List<Student> GetStudents()
+        {
+            return new List<Student>()
+        {
+            new Student { ID = 1001, Name = "Preety", Gender = "Female",
+                                         Branch = "CSE", Age = 20 },
+            new Student { ID = 1002, Name = "Rakshith", Gender = "Male",
+                                         Branch = "ETC", Age = 21  },
+            new Student { ID = 1003, Name = "Pranaya", Gender = "Male",
+                                         Branch = "CSE", Age = 21  },
+            new Student { ID = 1004, Name = "Anurag", Gender = "Male",
+                                         Branch = "CSE", Age = 20  },
+            new Student { ID = 1005, Name = "Hina", Gender = "Female",
+                                         Branch = "ETC", Age = 20 }
+            };
+        }
+           
+    }
+}
