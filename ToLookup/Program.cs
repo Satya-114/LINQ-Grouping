13a07dc5a87b2c4db9f44c8d1f9c15a9e1331bea
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 09:17:58 +0530

    Add files via upload

---
+++ ToLookup/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_ToLookup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using Method Syntax
+            var GroupByMS = Student.GetStudents().ToLookup(s => s.Branch);
+            //Using Query Syntax
+            var GroupByQS = (from std in Student.GetStudents()
+                             select std).ToLookup(x => x.Branch);
+            //It will iterate through each group
+            foreach (var group in GroupByMS)
+            {
+                Console.WriteLine(group.Key + " : " + group.Count());
+                //Iterate through each student of a group
+                foreach (var student in group)
+                {
+                    Console.WriteLine("  Name :" + student.Name + ", Age: " + student.Age + ", Gender :" + student.Gender);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
